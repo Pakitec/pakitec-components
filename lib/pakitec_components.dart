f8791235da0b4fe8708cc26fd3baff64d999c49b
leonardoalmeida <leonardo@pakitec.com.br>
2021-12-20 14:38:52 -0300

    add inoutField

--- lib/pakitec_components.dart
+++ lib/pakitec_components.dart
@@ -7,3 +7,4 @@ export 'src/widgets/input_calendar.dart';
 export 'src/widgets/input_zip_code.dart';
 export 'src/widgets/combo_field.dart';
 export 'src/widgets/dialogs.dart';
+export 'src/widgets/skeleton_indicator.dart';
