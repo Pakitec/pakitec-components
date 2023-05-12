7980d77b3273eb07f2ab5ac4cf2ef61d20b0244e
leonardo <leonardo@pakitec.com.br>
2023-05-12 19:37:23 -0300

    fix consts

--- lib/src/widgets/dialogs.dart
+++ lib/src/widgets/dialogs.dart
@@ -3,9 +3,6 @@ import 'package:fluttericon/font_awesome5_icons.dart';
 import 'package:pakitec_components/src/styles/main_style.dart';
 import '../../pakitec_components.dart';
 
-
-
-
 pakiShowQuestionYesNo(
     {required BuildContext context,
     required String message,
@@ -20,56 +17,45 @@ pakiShowQuestionYesNo(
       child: const Text('Não', style: TextStyle(color: Colors.white)),
       onPressed: onCancel);
   AlertDialog alert = AlertDialog(
-    title: Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: const [
-        Text('Atenção'),
-        PakiVerticalDiv(value: 5.0),
-        Icon(FontAwesome5.exclamation_circle),
+      title: const Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text('Atenção'),
+          PakiVerticalDiv(value: 5.0),
+          Icon(FontAwesome5.exclamation_circle),
+        ]),
+        PakiHorizontalDiv(value: 15.0),
+        Divider(height: 1.0, color: Colors.white)
       ]),
-      const PakiHorizontalDiv(value: 15.0),
-      const Divider(height: 1.0, color: Colors.white)
-    ]),
-    content: Text(message),
-    actions: [yes, no],
-    backgroundColor: secondaryColor,
-  );
+      content: Text(message),
+      actions: [yes, no],
+      backgroundColor: secondaryColor);
   showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return alert;
-    },
-  );
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      });
 }
 
-pakiShowSnackBar({required BuildContext context, required Widget content, required Color color, SnackBarAction?
-action}) {
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    content: content,
-    backgroundColor: color,
-    duration: const Duration(seconds: 5),
-    action: action,
-  ));
+pakiShowSnackBar(
+    {required BuildContext context, required Widget content, required Color color, SnackBarAction? action}) {
+  ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: content, backgroundColor: color, duration: const Duration(seconds: 5), action: action));
 }
 
 pakiShowSnackBarErrors(
-    {required BuildContext context, required String content, SnackBarAction? action,String? urlImage}) {
+    {required BuildContext context, required String content, SnackBarAction? action, String? urlImage}) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    content: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
+      content: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
         SizedBox(
             width: MediaQuery.of(context).size.width * 70 / 100,
             child: Text(
               content,
               style: const TextStyle(color: Colors.white),
             )),
-        Image.asset(urlImage ?? 'assets/images/bermuda_error.png', height: 60, width: MediaQuery.of(context).size
-            .width * 15 /
-            100)
-      ],
-    ),
-    backgroundColor: Colors.red,
-    duration: const Duration(seconds: 5),
-    action: action,
-  ));
+        Image.asset(urlImage ?? 'assets/images/bermuda_error.png',
+            height: 60, width: MediaQuery.of(context).size.width * 15 / 100)
+      ]),
+      backgroundColor: Colors.red,
+      duration: const Duration(seconds: 5),
+      action: action));
 }
