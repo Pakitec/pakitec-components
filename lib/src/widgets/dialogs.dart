cdaecbf40992e9fb1a36d926b88125f247fc448d
leonardo <leonardo@pakitec.com.br>
2023-05-18 19:18:06 -0300

    fix consts

--- lib/src/widgets/dialogs.dart
+++ lib/src/widgets/dialogs.dart
@@ -7,35 +7,42 @@ pakiShowQuestionYesNo(
     {required BuildContext context,
     required String message,
     required Function() onConfirm,
-    required Function() onCancel}) {
-  Widget yes = TextButton(
-      style: ButtonStyle(backgroundColor: MaterialStateProperty.all(buttonColor)),
-      child: const Text('Sim', style: TextStyle(color: Colors.white)),
-      onPressed: onConfirm);
-  Widget no = TextButton(
-      style: ButtonStyle(backgroundColor: MaterialStateProperty.all(buttonColor)),
-      child: const Text('Não', style: TextStyle(color: Colors.white)),
-      onPressed: onCancel);
-  AlertDialog alert = AlertDialog(
-      title: const Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text('Atenção'),
-          PakiVerticalDiv(value: 5.0),
-          Icon(FontAwesome5.exclamation_circle),
-        ]),
-        PakiHorizontalDiv(value: 15.0),
-        Divider(height: 1.0, color: Colors.white)
-      ]),
-      content: Text(message),
-      actions: [yes, no],
-      backgroundColor: secondaryColor);
+    required Function() onCancel,
+    bool? needsConfirm}) {
+  needsConfirm ??= true;
 
-  if (!context.mounted) return;
-  showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return alert;
-      });
+  if (!needsConfirm) {
+    onConfirm();
+  } else {
+    Widget yes = TextButton(
+        style: ButtonStyle(backgroundColor: MaterialStateProperty.all(buttonColor)),
+        child: const Text('Sim', style: TextStyle(color: Colors.white)),
+        onPressed: onConfirm);
+    Widget no = TextButton(
+        style: ButtonStyle(backgroundColor: MaterialStateProperty.all(buttonColor)),
+        child: const Text('Não', style: TextStyle(color: Colors.white)),
+        onPressed: onCancel);
+    AlertDialog alert = AlertDialog(
+        title: const Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text('Atenção'),
+            PakiVerticalDiv(value: 5.0),
+            Icon(FontAwesome5.exclamation_circle),
+          ]),
+          PakiHorizontalDiv(value: 15.0),
+          Divider(height: 1.0, color: Colors.white)
+        ]),
+        content: Text(message),
+        actions: [yes, no],
+        backgroundColor: secondaryColor);
+
+    if (!context.mounted) return;
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alert;
+        });
+  }
 }
 
 pakiShowSnackBar(
