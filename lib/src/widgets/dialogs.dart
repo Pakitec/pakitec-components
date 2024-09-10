f4b471ea1951d3206a9f319680796bd0b2fd57a2
leonardo <leonardo@pakitec.com.br>
2024-09-09 21:44:44 -0300

    add loading on question

--- lib/src/widgets/dialogs.dart
+++ lib/src/widgets/dialogs.dart
@@ -2,51 +2,121 @@ import 'package:flutter/material.dart';
 import 'package:pakitec_themes/pakitec_themes.dart';
 import 'divider.dart';
 
-pakiShowQuestionYesNo(
-    {required BuildContext context,
-    required String message,
-    required Function() onConfirm,
-    required Function() onCancel,
-    bool? isLoading,
-    bool? needsConfirm}) {
+// pakiShowQuestionYesNo(
+//     {required BuildContext context,
+//     required String message,
+//     required Function() onConfirm,
+//     required Function() onCancel,
+//     bool? isLoading,
+//     bool? needsConfirm}) {
+//   needsConfirm ??= true;
+//   isLoading ??= false;
+//
+//   if (!needsConfirm) {
+//     onConfirm();
+//   } else {
+//     Widget yes = isLoading
+//         ? const Center(child: CircularProgressIndicator())
+//         : TextButton(
+//             style: ButtonStyle(backgroundColor: MaterialStateProperty.all(pakiDefaultButtonColor)),
+//             onPressed: onConfirm,
+//             child: const Text('Sim', style: TextStyle(color: Colors.white)));
+//     Widget no = isLoading
+//         ? const Center(child: CircularProgressIndicator())
+//         : TextButton(
+//             style: ButtonStyle(backgroundColor: MaterialStateProperty.all(pakiDefaultButtonColor)),
+//             onPressed: onCancel,
+//             child: const Text('Não', style: TextStyle(color: Colors.white)));
+//     AlertDialog alert = AlertDialog(
+//         title: const Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+//           Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+//             Text('Atenção'),
+//             PakiVerticalDiv(width: 5.0),
+//             Icon(Icons.info),
+//           ]),
+//           PakiHorizontalDiv(height: 15.0),
+//           Divider(height: 1.0, color: Colors.white)
+//         ]),
+//         content: Text(message),
+//         actions: [yes, no],
+//         backgroundColor: pakiDefaultSecondaryColor);
+//
+//     if (!context.mounted) return;
+//     showDialog(
+//         context: context,
+//
+//         builder: (BuildContext context) {
+//           return alert;
+//         });
+//   }
+// }
+
+void pakiShowQuestionYesNo({
+  required BuildContext context,
+  required String message,
+  required Function() onConfirm,
+  required Function() onCancel,
+  bool? isLoading,
+  bool? needsConfirm,
+}) {
   needsConfirm ??= true;
   isLoading ??= false;
 
   if (!needsConfirm) {
     onConfirm();
   } else {
-    Widget yes = isLoading
-        ? const Center(child: CircularProgressIndicator())
-        : TextButton(
-            style: ButtonStyle(backgroundColor: MaterialStateProperty.all(pakiDefaultButtonColor)),
-            onPressed: onConfirm,
-            child: const Text('Sim', style: TextStyle(color: Colors.white)));
-    Widget no = isLoading
-        ? const Center(child: CircularProgressIndicator())
-        : TextButton(
-            style: ButtonStyle(backgroundColor: MaterialStateProperty.all(pakiDefaultButtonColor)),
-            onPressed: onCancel,
-            child: const Text('Não', style: TextStyle(color: Colors.white)));
-    AlertDialog alert = AlertDialog(
-        title: const Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text('Atenção'),
-            PakiVerticalDiv(width: 5.0),
-            Icon(Icons.info),
-          ]),
-          PakiHorizontalDiv(height: 15.0),
-          Divider(height: 1.0, color: Colors.white)
-        ]),
-        content: Text(message),
-        actions: [yes, no],
-        backgroundColor: pakiDefaultSecondaryColor);
-
-    if (!context.mounted) return;
+    // Exibir o Dialog
     showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return alert;
-        });
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder( // Permite que o diálogo atualize o estado
+          builder: (context, setState) {
+            // Botões que mudam dependendo do estado de isLoading
+            Widget yes = isLoading!
+                ? const Center(child: CircularProgressIndicator())
+                : TextButton(
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(pakiDefaultButtonColor)),
+              onPressed: () {
+                setState(() {
+                  isLoading = true; // Exibe o loading
+                });
+                onConfirm();
+              },
+              child: const Text('Sim', style: TextStyle(color: Colors.white)),
+            );
+
+            Widget no = isLoading!
+                ? const Center(child: CircularProgressIndicator())
+                : TextButton(
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(pakiDefaultButtonColor)),
+              onPressed: onCancel,
+              child: const Text('Não', style: TextStyle(color: Colors.white)),
+            );
+
+            return AlertDialog(
+              title: const Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Atenção'),
+                      SizedBox(width: 5.0),
+                      Icon(Icons.info),
+                    ],
+                  ),
+                  SizedBox(height: 15.0),
+                  Divider(height: 1.0, color: Colors.white),
+                ],
+              ),
+              content: Text(message),
+              actions: [yes, no],
+              backgroundColor: pakiDefaultSecondaryColor,
+            );
+          },
+        );
+      },
+    );
   }
 }
 
